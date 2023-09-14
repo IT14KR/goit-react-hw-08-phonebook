213a6c931d3e0d202c13381ca167c9a668a05e2c
IT14KR <131267129+IT14KR@users.noreply.github.com>
2023-09-14 14:11:03 +0000

    Deploying to gh-pages from @ IT14KR/goit-react-hw-08-phonebook@cfe6a56d639e2d51635818dd12ba32ec9a186da0 🚀

---
+++ static/js/415.ff288cb9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:function(e,t,r){r.r(t);var o=r(184);t.default=function(){return(0,o.jsx)(o.Fragment,{children:(0,o.jsx)("h1",{children:"Welcome to your private phonebook"})})}}}]);
+//# sourceMappingURL=415.ff288cb9.chunk.js.map
